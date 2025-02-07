1ab9b3b0ab0b8fdd4ac6c4ce9696271b8939d0c8
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2025-02-07 02:29:54 +0100

    Updating style for layout

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,31 @@
+// footer.component.ts
 import { Component } from '@angular/core';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  socialMediaLinks = [
+    { icon: ['fab', 'facebook'], link: 'https://facebook.com/blablahouse' },
+    { icon: ['fab', 'twitter'], link: 'https://twitter.com/blablahouse' },
+    { icon: ['fab', 'instagram'], link: 'https://instagram.com/blablahouse' },
+    { icon: ['fab', 'linkedin'], link: 'https://linkedin.com/company/blablahouse' },
+    { icon: ['fab', 'tiktok'], link: 'https://tiktok.com/@blablahouse' }
+  ];
 
+  quickLinks = [
+    { label: 'About Us', link: '/about' },
+    { label: 'Contact', link: '/contact' },
+    { label: 'FAQ', link: '/faq' },
+    { label: 'Careers', link: '/careers' }
+  ];
 }
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,30 +1,44 @@
-import {Component, effect, inject, OnInit} from '@angular/core';
-import {ButtonModule} from "primeng/button";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {ToolbarModule} from "primeng/toolbar";
-import {MenuModule} from "primeng/menu";
-import {CategoryComponent} from "./category/category.component";
-import {AvatarComponent} from "./avatar/avatar.component";
-import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
-import {MenuItem} from "primeng/api";
-import {ToastService} from "../toast.service";
-import {AuthService} from "../../core/auth/auth.service";
-import {User} from "../../core/model/user.model";
-import {PropertiesCreateComponent} from "../../landlord/properties-create/properties-create.component";
-import {SearchComponent} from "../../tenant/search/search.component";
-import {ActivatedRoute} from "@angular/router";
+// navbar.component.ts
+import { Component, effect, inject, OnInit } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from "primeng/button";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ToolbarModule } from "primeng/toolbar";
+import { MenuModule } from "primeng/menu";
+import { CarouselModule } from 'primeng/carousel';
+import { CategoryComponent } from "./category/category.component";
+import { AvatarComponent } from "./avatar/avatar.component";
+import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
+import { MenuItem } from "primeng/api";
+import { ToastService } from "../toast.service";
+import { AuthService } from "../../core/auth/auth.service";
+import { User } from "../../core/model/user.model";
+import { PropertiesCreateComponent } from "../../landlord/properties-create/properties-create.component";
+import { SearchComponent } from "../../tenant/search/search.component";
+import { ActivatedRoute } from "@angular/router";
 import dayjs from "dayjs";
+
+interface City {
+  name: string;
+  image: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
+    CommonModule,
+    RouterModule,
     ButtonModule,
     FontAwesomeModule,
     ToolbarModule,
     MenuModule,
+    CarouselModule,
     CategoryComponent,
-    AvatarComponent
+    AvatarComponent,
+    NgOptimizedImage
   ],
   providers: [DialogService],
   templateUrl: './navbar.component.html',
@@ -145,5 +159,87 @@ export class NavbarComponent implements OnInit {
         }
       }
     })
+  }
+
+  // New city slider properties
+  cities: City[] = [
+    {
+      name: 'Marrakech',
+      image: 'assets/images/marrakech.jpg',
+      description: 'Red City'
+    },
+    {
+      name: 'Paris',
+      image: 'assets/images/paris.jpg',
+      description: 'City of Love and Lights'
+    },
+    {
+      name: 'New York',
+      image: 'assets/images/newyork.jpg',
+      description: 'The City That Never Sleeps'
+    },
+    {
+      name: 'Tokyo',
+      image: 'assets/images/tokyo.jpg',
+      description: 'Modern Metropolis'
+    },
+    {
+      name: 'Barcelona',
+      image: 'assets/images/barcelona.jpg',
+      description: 'Architectural Wonder'
+    },
+    {
+      name: 'Istanbul',
+      image: 'assets/images/istanbul.jpg',
+      description: 'City on Two Continents'
+    },
+    {
+      name: 'Singapore',
+      image: 'assets/images/singapore.jpg',
+      description: 'Garden City'
+    },
+    {
+      name: 'Cape Town',
+      image: 'assets/images/capetown.jpg',
+      description: 'The Mother City'
+    },
+    {
+      name: 'Sydney',
+      image: 'assets/images/sydney.jpg',
+      description: 'Harbour City'
+    },
+    {
+      name: 'Rio de Janeiro',
+      image: 'assets/images/riodejaneiro.jpg',
+      description: 'The Marvellous City'
+    },
+    {
+      name: 'Dubai',
+      image: 'assets/images/dubai.jpg',
+      description: 'City of Gold'
+    }
+  ];
+  responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+  // Method to handle city selection
+  selectCity(city: City) {
+    this.location = city.name;
+    this.openNewSearch();
   }
 }
